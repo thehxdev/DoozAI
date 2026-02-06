572e79e27ee65a958632cf0431e1ab79324d09d7
thehxdev <hossein.khosravi.ce@gmail.com>
2026-02-06 22:14:53 +0330

    added wasm related stuff
    Signed-off-by: thehxdev <hossein.khosravi.ce@gmail.com>

---
+++ build_wasm.bash
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+if ! command -v emcc >/dev/null; then
+    echo "[ERROR] filed to find 'emcc'"
+    echo "[INFO] install emscripten toolchain from https://emscripten.org/docs/getting_started/downloads.html"
+    exit 1
+fi
+
+SELF_PATH=$(realpath "$(dirname "$0")")
+DOOZ_RAYLIB_WASM="$SELF_PATH/raylib-wasm"
+
+emcc -O2 -Wall \
+    -I. -I"$DOOZ_RAYLIB_WASM/include" \
+    -L. -L"$DOOZ_RAYLIB_WASM/lib" \
+    -s USE_GLFW=3 -s ASYNCIFY -DPLATFORM_WEB \
+    --shell-file "$SELF_PATH/wasm/minshell.html" \
+    --embed-file "$SELF_PATH/assets/board.png" \
+    -o "$SELF_PATH/game.html" \
+    "$SELF_PATH/src/build.c" \
+    "$DOOZ_RAYLIB_WASM/lib/libraylib.a"
